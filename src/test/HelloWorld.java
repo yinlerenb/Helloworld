5e25bc3e64dc702251e703d66fa722fb769457eb
yinlerenb <the.nexus2333@gmail.com>
2020-05-11 14:26:37 +0800

    test

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -2,6 +2,6 @@ package test;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("HelloWorld!");
     }
 }
